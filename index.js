ddaa4165e7f5de5e6cd95daef976e6b710f432a7
Daniel Hallada <hallada.daniel@gmail.com>
2018-12-15 19:04:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var counter = []
+  for (let i = 0; i < musicians.length; i++) {
+  counter.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+  return counter
+}
+
+function johnLennonFacts (facts) {
+  var factstest = []
+  let i = 0
+  while (i < facts.length) {
+    factstest.push(`${facts[i]}!!!`)
+  }
+  return factstest
+}
